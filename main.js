a68115d2bdc5fd9b79a6cf94006ebea26c0643dd
MrLexx1 <muhilman606@gmail.com>
2024-11-04 11:10:39 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//  ~~~~~ TUGAS KELIMA ~~~~~ \\
+
+function hitungDiskon(harga, diskon) {
+  let jumlahDiskon = harga * (diskon / 100);
+  let hargaSetelahDiskon = harga - jumlahDiskon;
+  return hargaSetelahDiskon;
+}
+
+let hargaSetelahDiskon = hitungDiskon(100000, 20);
+console.log(hargaSetelahDiskon);
